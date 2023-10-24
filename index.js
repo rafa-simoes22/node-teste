707d78194c5cb0550e0944f38798051edcad25dc
Rafa <rafaelasimoes2203@gmail.com>
2023-10-24 20:54:17 -0300

    acessando apartir do html

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const hostname = '192.168.56.1';
-const port = 3003;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('ola')
-});
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-server.listen(port,hostname, () => {
-    console.log('Servidor OK');
-});
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use('/', router);
+app.listen(process.env.port || 3003);
